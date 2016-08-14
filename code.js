11e1e146c2400a3200148713e9cbe7787e1cf767
Zhentian Wan <zhentian.wan@tecnotree.com>
2016-08-14 14:04:49 +0300

    02-flatMap

--- code.js
+++ code.js
@@ -1,19 +1,20 @@
 var quakes = Rx.Observable.create(function(observer){
     window.eqfeed_callback = function(response){
-        var quakes = response.features;
-        console.log("quakes:", JSON.stringify(quakes, null, 2));
-        quakes.forEach(function(quake){
-            observer.onNext(quake)
-        })
-    }
-
+        observer.onNext(response);
+        observer.onCompleted();
+    };
     loadJSONP(QUAKE_URL);
+}).flatMap(function transform(dataset){
+    return Rx.Observable.from(dataset.features);
+}).map(function(quake){
+    return {
+        lat: quake.geometry.coordinates[1],
+        lng: quake.geometry.coordinates[0],
+        size: quake.properties.mag * 10000
+    }
 });
 
 quakes.subscribe(function(quake){
-    var coords = quake.geometry.coordinates;
-    var size = quake.properties.mag * 1000;
-
-    L.circle([coords[1], coords[0]], size).addTo(map)
+    L.circle([quake.lat, quake.lng], quake.size).addTo(map)
 });
 
